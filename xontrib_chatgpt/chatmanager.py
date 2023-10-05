776cf5ba5ccd955863cfe500f02374e79ba404b7
jpal91 <pallanschja@gmail.com>
2023-10-05 17:14:10 -0400

    docstrings, changed func names

--- xontrib_chatgpt/chatmanager.py
+++ xontrib_chatgpt/chatmanager.py
@@ -12,12 +12,16 @@ from xonsh.lazyasd import LazyObject, lazyobject
 from xontrib_chatgpt.chatgpt import ChatGPT
 from xontrib_chatgpt.lazyobjs import _FIND_NAME_REGEX
 
-FIND_NAME_REGEX: Pattern = LazyObject(_FIND_NAME_REGEX, globals(), 'FIND_NAME_REGEX')
+FIND_NAME_REGEX: Pattern = LazyObject(_FIND_NAME_REGEX, globals(), "FIND_NAME_REGEX")
+
+
 # PARSER: ArgumentParser = LazyObject(_cm_parse, globals(), 'PARSER')
 @lazyobject
 def PARSER():
     from xontrib_chatgpt.args import _cm_parse
+
     return _cm_parse()
+
 
 class ChatManager:
     """Class to manage multiple chats"""
@@ -27,109 +31,186 @@ class ChatManager:
         self._current: Optional[int] = None
         self._update_inst_dict()
 
-    def __call__(self, args: list[str], stdin: TextIO=None):
+    def __call__(self, args: list[str], stdin: TextIO = None):
+        """Main method to interact with ChatManager via xonsh aliases"""
         if args:
             pargs = PARSER.parse_args(args)
         elif stdin:
             pargs = PARSER.parse_args(stdin.read().strip().split())
         else:
             return PARSER.print_help()
-        
+
         if pargs.C:
             if self._current is None:
-                return 'No active chat!'
+                return "No active chat!"
             else:
-                return str(self._instances[self._current]['inst'])
-        
-        if pargs.cmd == 'add':
+                return str(self._instances[self._current]["inst"])
+
+        if pargs.cmd == "add":
             return self.add(pargs.name[0])
-        elif pargs.cmd in ['list', 'ls']:
+        elif pargs.cmd in ["list", "ls"]:
             return self.ls(saved=pargs.saved)
-        elif pargs.cmd == 'load':
+        elif pargs.cmd == "load":
             return self.load(pargs.name[0])
-        elif pargs.cmd == 'save':
+        elif pargs.cmd == "save":
             return self.save(chat_name=pargs.name, mode=pargs.mode)
-        elif pargs.cmd == 'print':
+        elif pargs.cmd == "print":
             return self.print_chat(chat_name=pargs.name, n=pargs.n, mode=pargs.mode)
-        elif pargs.cmd == 'help':
+        elif pargs.cmd == "help":
             return self.help(tgt=pargs.target)
         else:
             return PARSER.print_help()
 
     def add(self, chat_name: str) -> str:
-        """Create new chat instance"""
+        """Create new chat instance
+
+        Parameters
+        ----------
+        chat_name : str
+            Name of the chat instance to create
+
+        Returns
+        -------
+        str
+        """
         if chat_name in self.chat_names():
-            return 'Chat with that name already exists!'
+            return "Chat with that name already exists!"
         inst = ChatGPT(alias=chat_name, managed=True)
         XSH.ctx[chat_name] = inst
-        self._instances[hash(inst)]['name'] = chat_name
-        return f'Created new chat {chat_name}'
+        self._instances[hash(inst)]["name"] = chat_name
+        return f"Created new chat {chat_name}"
 
     def ls(self, saved: bool = False) -> None:
-        """List all active chats or saved chats"""
+        """List all active chats or saved chats
+
+        Parameters
+        ----------
+        saved : bool, optional
+            Whether to list saved chats or active chats, by default False
+
+        Returns
+        -------
+        None
+        """
         if saved:
-            print('Saved chats:')
-            [print(f'  {f}') for f in self._find_saved()]
+            print("Saved chats:")
+            [print(f"  {f}") for f in self._find_saved()]
             return
-        
+
         for inst in self._instances.values():
             print(f'Name: {inst["name"]}')
-            print(str(inst['inst']))
-            print('')
+            print(str(inst["inst"]))
+            print("")
 
     def load(self, path_or_name: str) -> str:
-        """Load a conversation from a path or a saved chat name"""
+        """Load a conversation from a path or a saved chat name
+
+        Parameters
+        ----------
+        path_or_name : str
+            Path to a conversation file or name of a saved chat
+
+        Returns
+        -------
+        str
+        """
         if os.path.exists(path_or_name):
-            path, name = path_or_name, FIND_NAME_REGEX.sub(r'\1', os.path.basename(path_or_name))
+            path, name = path_or_name, FIND_NAME_REGEX.sub(
+                r"\1", os.path.basename(path_or_name)
+            )
         else:
             path, name = self._find_path_from_name(path_or_name)
-        
+
         curr_names = self.chat_names()
-        
+
         if name in curr_names:
             idx = 0
             while name + str(idx) in curr_names:
                 idx += 1
-            print(f'Chat with name {name} already loaded. Updating to {name + str(idx)}')
+            print(
+                f"Chat with name {name} already loaded. Updating to {name + str(idx)}"
+            )
             name += str(idx)
-            
+
         inst = ChatGPT.fromconvo(path, alias=name, managed=True)
         XSH.ctx[name] = inst
-        self._instances[hash(inst)]['name'] = name
+        self._instances[hash(inst)]["name"] = name
 
-        return f'Loaded chat {name} from {path}'
+        return f"Loaded chat {name} from {path}"
 
-    def save(self, chat_name: str = '', mode: str = 'text') -> Optional[str]:
+    def save(self, chat_name: str = "", mode: str = "text") -> Optional[str]:
+        """Save a conversation to a file
+
+        Parameters
+        ----------
+        chat_name : str, optional
+            Name of the chat to save. Defaults to last used/current chat, by default ''
+        mode : str, optional
+            Mode to save the conversation. Defaults to 'text', other option is 'json'
+
+        Returns
+        -------
+        Optional[str]
+        """
+
         if not chat_name and not self._current:
-            return 'No active chat!'
+            return "No active chat!"
         elif not chat_name:
             chat = self._instances[self._current]
         else:
             try:
                 chat = self._instances[hash(XSH.ctx[chat_name])]
             except KeyError:
-                return f'No chat with name {chat_name} found.'
+                return f"No chat with name {chat_name} found."
 
-        return chat['inst'].save_convo(mode=mode)
+        return chat["inst"].save_convo(mode=mode)
 
+    def print_chat(
+        self, chat_name: str = "", n: int = 10, mode: str = "color"
+    ) -> Optional[str]:
+        """Print a conversation
 
-    def print_chat(self, chat_name: str = '', n: int = 10, mode: str = 'color') -> Optional[str]:
+        Parameters
+        ----------
+        chat_name : str, optional
+            Name of the chat to print. Defaults to last used/current chat, by default ''
+        n : int, optional
+            Number of conversations to print, by default 10
+        mode : str, optional
+            Mode to print the conversation. Defaults to 'color', other options are 'no-color' and 'json'
+
+        Returns
+        -------
+        Optional[str]
+        """
+
         if not chat_name and not self._current:
-            return 'No active chat!'
+            return "No active chat!"
         elif not chat_name:
             chat = self._instances[self._current]
         else:
             try:
                 chat = self._instances[hash(XSH.ctx[chat_name])]
             except KeyError:
-                return f'No chat with name {chat_name} found.'
-        
-        chat['inst'].print_convo(n=n, mode=mode)
+                return f"No chat with name {chat_name} found."
 
-    def help(self, tgt: str = '') -> None:
+        chat["inst"].print_convo(n=n, mode=mode)
+
+    def help(self, tgt: str = "") -> Optional[str]:
+        """Print help information on the instance, methods, or xontrib
+
+        Parameters
+        ----------
+        tgt : str, optional
+            Target to print help information on. Defaults to chat-manager, by default ''
+
+        Returns
+        -------
+        Optional[str]
+        """
         if not tgt:
             return self._usage_str()
-        
+
         if hasattr(self, tgt):
             XSH.help(getattr(self, tgt))
         elif hasattr(ChatGPT, tgt):
@@ -138,51 +219,60 @@ class ChatManager:
             PARSER.print_help()
 
     def chat_names(self) -> list[str]:
-        return [inst['name'] for inst in self._instances.values()]
+        """Returns chat names for current conversations"""
+        return [inst["name"] for inst in self._instances.values()]
 
     def _find_saved(self) -> list[str]:
-        def_dir = os.path.join(XSH.env['XONSH_DATA_DIR'], 'chatgpt')
-        return [f for f in os.listdir(def_dir) if os.path.isfile(os.path.join(def_dir, f))]
-    
+        """Returns a list of saved chat files in the default directory"""
+        def_dir = os.path.join(XSH.env["XONSH_DATA_DIR"], "chatgpt")
+        return [
+            f for f in os.listdir(def_dir) if os.path.isfile(os.path.join(def_dir, f))
+        ]
+
     def _find_path_from_name(self, name: str) -> tuple[str, str]:
-        def_dir = XSH.env['XONSH_DATA_DIR']
+        """Finds a saved chat file from user input if it's not a path"""
+        def_dir = XSH.env["XONSH_DATA_DIR"]
         saved = self._find_saved()
 
         if name in saved:
-            return os.path.join(def_dir, 'chatgpt', name), FIND_NAME_REGEX.sub(r'\1', name)
-        
+            return os.path.join(def_dir, "chatgpt", name), FIND_NAME_REGEX.sub(
+                r"\1", name
+            )
+
         names_saved = [
-            (n, f) for n, f in
-            [(FIND_NAME_REGEX.sub(r'\1', f), f) for f in saved]
+            (n, f)
+            for n, f in [(FIND_NAME_REGEX.sub(r"\1", f), f) for f in saved]
             if n == name
         ]
 
         if not names_saved:
-            raise FileNotFoundError(f'No chat with name {name} found.')
+            raise FileNotFoundError(f"No chat with name {name} found.")
         elif len(names_saved) > 1:
             chat_name, file = self._choose_from_multiple(names_saved)
         else:
             chat_name, file = names_saved[0]
-        
-        
-        return os.path.join(def_dir, 'chatgpt', file), chat_name
-    
+
+        return os.path.join(def_dir, "chatgpt", file), chat_name
+
     def _choose_from_multiple(self, chats: list[tuple[str, str]]) -> tuple[str, str]:
-        print('Multiple choices found, please choose from:')
-        [print(f'  {i + 1}. {n[0]}') for i, n in enumerate(chats)]
-        
+        """If multiple chats are found, allows the user to choose from them"""
+        print("Multiple choices found, please choose from:")
+        [print(f"  {i + 1}. {n[0]}") for i, n in enumerate(chats)]
+
         while True:
             max_choice = len(chats)
-            choice = input(f'Number (1 - {max_choice}): ')
+            choice = input(f"Number (1 - {max_choice}): ")
 
             try:
                 choice = int(choice)
             except ValueError:
-                print('Invalid input, please enter a number.')
+                print("Invalid input, please enter a number.")
                 continue
 
             if not 0 < choice <= max_choice:
-                print(f'Invalid input, please enter a number between 1 and {max_choice}.')
+                print(
+                    f"Invalid input, please enter a number between 1 and {max_choice}."
+                )
                 continue
 
             return chats[choice - 1]
@@ -191,37 +281,36 @@ class ChatManager:
         """Finds all active instances (active convos) in the global space and updates the internal dict"""
         for key, inst in XSH.ctx.copy().items():
             if isinstance(inst, ChatGPT):
-                
                 self._instances[hash(inst)] = {
-                    'name': key,
-                    'alias': inst.alias,
-                    'inst': weakref.proxy(inst),
+                    "name": key,
+                    "alias": inst.alias,
+                    "inst": weakref.proxy(inst),
                 }
-        
+
         if self._current not in self._instances:
             self._current = None
-    
-    def _on_chat_create(self, inst: ChatGPT) -> None:
+
+    def on_chat_create_handler(self, inst: ChatGPT) -> None:
         """Handler for on_chat_create. Updates the internal dict with the new chat instance."""
         self._current = hash(inst)
         self._instances[hash(inst)] = {
-            'name': '',
-            'alias': inst.alias,
-            'inst': weakref.proxy(inst),
+            "name": "",
+            "alias": inst.alias,
+            "inst": weakref.proxy(inst),
         }
-    
-    def _on_chat_destroy(self, inst_hash: int) -> None:
+
+    def on_chat_destroy_handler(self, inst_hash: int) -> None:
         """Handler for on_chat_destroy. Removes the chat instance from the internal dict."""
         if inst_hash in self._instances:
             del self._instances[inst_hash]
-        
+
         if inst_hash == self._current:
             self._current = None
-    
-    def _on_chat_used(self, inst_hash: int) -> None:
+
+    def on_chat_used_handler(self, inst_hash: int) -> None:
         """Handler for on_chat_used. Updates the current chat instance."""
         self._current = inst_hash
-    
+
     def _usage_str(self) -> str:
         """Returns a usage string for the xontrib."""
         usage = """
